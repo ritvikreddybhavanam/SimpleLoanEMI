7b47f395b8c32f700f426c0166cfa500e24f189d
Ritvik <4ritvikreddy5@gmail.com>
2025-12-10 12:48:16 +0530

    add colors to seperate file

---
+++ src/Colors.java
@@ -0,0 +1,9 @@
+class Colors {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String MAGENTA = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+}
